95a4aa1eaa4020aa84376103b81a3f43ec110cf6
makotokato <ddt.mkato@gmail.com>
2021-05-18 11:28:13 +0900

    関数ヘッダの追加

--- meikaiJava/src/chapter01/practice1_1.java
+++ meikaiJava/src/chapter01/practice1_1.java
@@ -1,16 +1,16 @@
 package chapter01;
 
 /**
- * 問題文
+ *
  * @author システム開発部
+ *
+ * 演習1-1
+ * プログラム中の文の終端を示すセミコロン；が欠如しているとどうなるか。プログラムをコンパイルして検証せよ。
  *
  */
 public class practice1_1 {
 
 	public static void main(String[] args) {
-
-		//演習1-1
-		//プログラム中の文の終端を示すセミコロン；が欠如しているとどうなるか。プログラムをコンパイルして検証せよ。
 
 		//;がないため式文として認識されずコンパイルエラーになる。
 		//System.out.println("HelloWorld")
