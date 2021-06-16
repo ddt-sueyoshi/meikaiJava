7e70d7380090d55ac388f2d56644e7e8391e8966
makotokato <ddt.mkato@gmail.com>
2021-06-16 21:23:55 +0900

    演習問題7-27~31

---
+++ meikaiJava/src/chapter07/practice7_27.java
@@ -0,0 +1,16 @@
+package chapter07;
+/**
+ *
+ * @author システム開発部
+ *演習問題7-27
+ *List7-20のプログラムは三つの配列x, y, zの行数および列数が同一であることを前提としている。
+ *三つの配列の要素数が等しければ加算を行ってtrueを返し、等しくなければ加算を行わずにfalseを返すメソッドに書きかえよ。
+ */
+public class practice7_27 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
---
+++ meikaiJava/src/chapter07/practice7_29.java
@@ -0,0 +1,10 @@
+package chapter07;
+
+public class practice7_29 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
