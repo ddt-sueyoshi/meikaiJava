3c516a48e3c841984290de85fdac28966d2bb92b
makotokato <ddt.mkato@gmail.com>
2021-09-15 20:01:04 +0900

    sueyoshi_レビュー指摘修正

--- meikaiJava/src/chapter05/practice5_6.java
+++ meikaiJava/src/chapter05/practice5_6.java
@@ -14,12 +14,12 @@ public class practice5_6 {
 		float number = 0.0F;
 
 		//ヘッダを表示
-		System.out.println("  float   int  \n----------------");
+		System.out.println("  float     int  \n----------------------");
 
 		//整数が0から1000まで1ずつ増える分繰り返す
 		for (int i = 0; i <= 1000; i++) {
-			System.out.printf("%7.3f", number); //フォーマットを指定し（7桁以上、小数部は3桁）float型の表示
-			System.out.printf("%7.3f", ((double) i / 1000)); //フォーマットを指定し（7桁以上、小数部は3桁）double型の表示
+			System.out.printf("%10f", number); //フォーマットを指定し（小数部は7桁）float型の表示
+			System.out.printf("%10f", ((float) i / 1000)); //フォーマットを指定し（小数部は7桁）double型の表示
 			System.out.println(); //改行
 			number += 0.001; //double型をfloat型に合わせて加算
 		}
