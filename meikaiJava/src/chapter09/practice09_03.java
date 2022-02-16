d52656b7d3a6822dd9793798eea4749e908c84cf
USER <>
2022-02-16 20:50:37 +0900

    sueyoshi_fix comment

--- meikaiJava/src/chapter09/practice09_03.java
+++ meikaiJava/src/chapter09/practice09_03.java
@@ -5,6 +5,8 @@ package chapter09;
  * @author user 第８章で作成した《銀行口座クラス（第２版》(p.277)に、口座開設日のフィールドとtoStringメソッドを追加せよ。
  *         ※コンストラクタを変更したり、口座開設日のゲッタ（口座開設日フィールドが参照する日付インスタンスのコピーを返す）などのメソッドを追加したりすること。
  */
+
+/** 銀行口座クラス */
 class Account {
 	private String name; // 口座名義
 	private String no; // 口座番号
@@ -58,15 +60,15 @@ class Account {
 public class practice09_03 {
 
 	public static void main(String[] args) {
-		// 銀行口座を作成
+		// adachi, houjou ２つの銀行口座を作成
 		Account adachi = new Account("安達泰盛","123456",1000,2000,11,11);
 		Account houjou = new Account("北条高時","234567",2000,2012,5,3);
 		
-		// 残高を引き出す, 預金する
+		// adachiが残高を引き出す, houjouが残高を預金する
 		adachi.withdraw(200);
 		houjou.deposit(500);
 		
-		// 口座情報を出力
+		// ２つの口座情報を出力
 		System.out.println("■"+adachi.getName()+"の口座");
 		System.out.println("     口座番号："+adachi.getNo());
 		System.out.println("     預金残高："+adachi.getBalance());
